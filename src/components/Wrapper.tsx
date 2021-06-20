8982fa1f8c92f9f80bb1049b742c38a31792a297
Alan <aln447@gmail.com>
2021-06-20 22:36:35 +0200

    Add the base types for the settings
    The settings will only consists of the Warriors and the Sprint settings.
    Used types instead of interfaces since we're not really planning for
    these to be extendable.

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,6 +2,23 @@ import { Settings } from './Settings';
 import react from 'react';
 import { Counter } from './Counter';
 
+type TWarrior = {
+    name: string;
+    slug: string;
+    color: string;
+}
+
+type TSprint = {
+    name: string;
+    length: number;
+    pointsPerWorkday: number;
+}
+
+type TSettings = {
+    warriors: TWarrior[];
+    sprint: TSprint;
+}
+
 export const Wrapper = () => {
     return <div>
         <h2>I'm the wrapper</h2>
