147266461e134f8ea8cf6259533cbfc957ad2d5b
Alan <aln447@gmail.com>
2021-07-08 19:53:49 +0200

    Create a component for the WarriorForm
    A small form that will be used in the hook form loop

---
+++ src/components/WarriorForm.tsx
@@ -0,0 +1,24 @@
+import react from 'react';
+import { TSettings } from './Wrapper';
+import { useForm, UseFormRegister } from 'react-hook-form';
+import { TextInput } from './inputs/TextInput';
+import { NumberInput } from './inputs/NumberInput';
+
+type TWarriorFormProps = {
+    register: UseFormRegister<TSettings>;
+    index: number;
+};
+
+export const WarriorForm = ({ index, register }: TWarriorFormProps) => {
+    return <div>
+        <label htmlFor={`warriors.${index}.name`}>
+            <TextInput name={`warriors.${index}.name`} register={register} />
+        </label>
+        <label htmlFor={`warriors.${index}.pointsPerSprint`}>
+            <TextInput name={`warriors.${index}.pointsPerSprint`} register={register} />
+        </label>
+        <label htmlFor={`warriors.${index}.color`}>
+            <TextInput name={`warriors.${index}.color`} register={register} />
+        </label>
+    </div>
+}
