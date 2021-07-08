cce81091cc4313c605259e76bb455bbba3667862
Alan <aln447@gmail.com>
2021-07-08 20:18:35 +0200

    Remove the elements oculling the wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { Wrapper } from './components/Wrapper';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        This is a cool counter
-      </header>
-      <Wrapper />
-    </div>
-  );
+  return <Wrapper />;
 }
 
 export default App;
