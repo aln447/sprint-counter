a8e3cc5d7d685d3df12b52cc3b4f1458532c0c5c
Alan <aln447@gmail.com>
2021-08-22 22:41:09 +0200

    Add base styling to the counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -3,20 +3,25 @@ import { getAllJSDocTags } from 'typescript';
 import { TSettings, TWarrior } from '../Wrapper';
 import { DayTable } from '../day-table/DayTable';
 import { ResultSet } from './ResultSet';
+import styled from 'styled-components';
 
 type CounterProps = {
     settings: TSettings;
 };
+
+const StyledCounter = styled.div`
+    padding: 15px;
+`;
 
 const getDayKeyword = (length: number) => length === 1 ? 'day' : 'days';
 
 export const Counter = ({ settings: { sprint: { name, length }, warriors: initialWarriors } }: CounterProps) => {
     const [warriors, setWarriors] = useState<TWarrior[]>(initialWarriors);
 
-    return <div>
+    return <StyledCounter>
         <h1>{name}</h1>
         <h3>Sprint length: {length} {getDayKeyword(length)}</h3>
         <DayTable warriors={warriors} setWarriors={setWarriors} length={length} />
         <ResultSet />
-    </div>
+    </StyledCounter>
 }
