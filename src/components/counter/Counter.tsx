bad1e6a0aa5588e35483ab3d79eb3308fcc574e2
Alan <aln447@gmail.com>
2021-08-22 20:32:59 +0200

    Print the base sprint data

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,10 +1,16 @@
 import react from 'react';
+import { getAllJSDocTags } from 'typescript';
 import { TSettings } from '../Wrapper';
 
 type CounterProps = {
-    settings?: TSettings;
+    settings: TSettings;
 };
 
-export const Counter = ({ settings }: CounterProps) => {
-    return <div>I'm the counter</div>
+const getDayKeyword = (length: number) => length === 1 ? 'day' : 'days';
+
+export const Counter = ({ settings: { sprint: { name, length }, warriors } }: CounterProps) => {
+    return <div>
+        <h1>{name}</h1>
+        <h3>Sprint length: {length} {getDayKeyword(length)}</h3>
+    </div>
 }
